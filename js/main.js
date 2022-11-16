f27a2a0eff839db4f720616dc8ba79c27f56183a
Erik Henrique <erikhrodrigues@gmail.com>
2022-11-16 10:22:59 -0300

    Comentando código e inserindo 'catch' na API

--- js/main.js
+++ js/main.js
@@ -2,18 +2,20 @@ let title = document.querySelector("#title");
 let result = document.querySelector("#result");
 let btn = document.querySelector("#adviceBtn");
 
+//Escutar botão
 btn.addEventListener("click", pegarConselho);
+
+//Mostrar mensagem
 async function pegarConselho() {
     try {
         let capturar = await fetch("https://api.adviceslip.com/advice").then(r => r.json());
         let resultado = capturar.slip
         title.innerHTML = `ADVICE #${resultado.id}`
         result.innerHTML = `"${resultado.advice}"`
-    } catch {
-
-    } finally {
-        console.log("fim")
+    } catch (erro){
+        result.innerHTML = `Ops, aconteceu um erro! <br> Serviço temporareamente fora do ar.`
     }
 }
 
+//Primeiro conselho
 pegarConselho();
